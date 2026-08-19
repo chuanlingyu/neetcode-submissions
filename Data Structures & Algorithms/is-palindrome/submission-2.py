b33adb8388162af1c325fc4badbb8e0b8f18754b
Chuanling Yu <87615730+chuanlingyu@users.noreply.github.com>
2026-08-19 15:01:06 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.replace(" ", "").casefold()
+        i = 0
+        j = len(s) - 1
+        while True:
+            while i < j and not (s[i].isnumeric() or s[i].isalpha()):
+                i += 1
+
+            while j > i and not (s[j].isnumeric() or s[j].isalpha()):
+                j -= 1
+
+            if i >= j:
+                break
+
+            if s[i] != s[j]:
+                return False
+            
+            i += 1
+            j -= 1
+
+        return True
